79375d1453801d53bfd4738b367723c8727ac212
Herman Slatman <hermanslatman@hotmail.com>
2020-08-08 14:00:15 +0200

    Add basics for handling MUD Files

---
+++ README.md
@@ -0,0 +1,41 @@
+# MUD File Server
+
+An example MUD File Server using Caddy as the web server
+
+## Description
+
+Manufacturer Usage Descriptions (MUDs) allow manufacturers of IoT equipment to specify the intended network communication patterns of the devices they manufacture. 
+The access control policies described in a MUD file allow network controllers to automatically enforce rules on the device, resulting in devices only being allowed to communicate within the boundaries of the access control policies.
+
+This repository contains an implementation of a MUD File Server based on the Caddy web server.
+MUD File Servers are responsible for serving MUD Files and their signatures, which can be retrieved by MUD Controllers when an IoT device emits a MUD URL.
+
+## Build
+
+Because the repository is currently private, we need to instruct Go that the module is private when trying to build a version of running the application:
+
+```bash
+# build the server 
+$ GOPRIVATE="github.com/hslatman/muds" go build cmd/main.go -o muds
+```
+
+## Usage
+
+```bash
+# run the server directly from Go code
+$ GOPRIVATE="github.com/hslatman/muds" go run cmd/main.go
+# run the server from compiled binary
+$ ./muds
+```
+
+## TODO
+
+* Change from Path Matcher to File Matcher?
+* Make mud-file-server work for serving MUD Files
+* Test TLS (local CA?)
+* Add example MUD including signature
+* Do we need some kind of abstract file system handling?
+* Do we need a simple database?
+* Implement a simple overview page of MUDs available?
+* Implement a MUD viewer to open the visualize the available MUDs?
+* Implement basic statistics about files requested?
--- pkg/mud/mud.go
+++ pkg/mud/mud.go
@@ -1,17 +1,55 @@
 package mud
 
-import "github.com/caddyserver/caddy/v2"
+import (
+	"net/http"
+
+	"github.com/caddyserver/caddy/v2"
+	"github.com/caddyserver/caddy/v2/modules/caddyhttp"
+)
 
 func init() {
-	caddy.RegisterModule(MudFileServer{})
+	caddy.RegisterModule(FileServer{})
 }
 
-type MudFileServer struct {
+// FileServer implements a MUD File Server responder for Caddy.
+type FileServer struct {
+	// The path to the root directory with MUD files.
+	// Default is `{http.vars.root}` if set; current working directory otherwise.
+	Root string `json:"root,omitempty"`
 }
 
-func (MudFileServer) CaddyModule() caddy.ModuleInfo {
+// CaddyModule returns the Caddy module information.
+func (FileServer) CaddyModule() caddy.ModuleInfo {
 	return caddy.ModuleInfo{
-		ID:  "http.handlers.mud",
-		New: func() caddy.Module { return new(MudFileServer) },
+		ID:  "http.handlers.mud_file_server",
+		New: func() caddy.Module { return new(FileServer) },
 	}
+}
+
+// Provision sets up the MUD File Server responder.
+func (m *FileServer) Provision(ctx caddy.Context) error {
+
+	if m.Root == "" {
+		m.Root = "{http.vars.root}"
+	}
+
+	return nil
+}
+
+// ServeHTTP is the core handler for the MUD File Server.
+func (m *FileServer) ServeHTTP(w http.ResponseWriter, r *http.Request, next caddyhttp.Handler) error {
+
+	w.Header().Set("server", "MUD File Server") // TODO: make this optional / configurable? Add version?
+
+	// TODO: determine requested file
+	// TODO: validate file exists; is no directory; etc.
+	// TODO: determine the type of file requested (MUD vs. its signature)
+	// TODO: validate file has valid signature (configurable?)
+	// TODO: validate file is valid MUD (configurable?)
+	// TODO: respond with the file contents and set appropriate headers
+
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("HELLO from MUD File Server!"))
+
+	return nil
 }
